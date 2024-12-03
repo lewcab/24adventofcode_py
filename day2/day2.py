90b20de4b0bd1615b7627999b07bc49b99e6289e
lewcab <lewcabal@gmail.com>
2024-12-03 11:44:06 -0800

    day 2 part 2 done

--- day2/day2.py
+++ day2/day2.py
@@ -8,7 +8,9 @@ def main():
         row = [int(x) for x in row]
         mat.append(row)
 
+    print(f"{len(mat)} Reports to check ...")
     print(f"Part 1: {part1(mat)}")
+    print(f"Part 2: {part2(mat)}")
 
 
 def part1(mat: list) -> int:
@@ -20,28 +22,31 @@ def part1(mat: list) -> int:
     return total_safe
 
 
+def part2(mat: list) -> int:
+    total_safe = 0
+    for report in mat:
+        if any([is_safe(remove_index(report, i)) for i in range(len(report))]):
+            total_safe += 1
+
+    return total_safe
+
+
 def is_safe(report: list) -> bool:
-    increasing = report[0] < report[-1]
-
-    prev = report[0]
-    for i in range(1, len(report)):
-        if increasing:
-            if report[i] < prev:
-                return False
-            diff = abs(report[i] - prev)
-            if not (1 <= diff <= 3):
-                return False
-
-        else:
-            if report[i] > prev:
-                return False
-            diff = abs(report[i] - prev)
-            if not (1 <= diff <= 3):
-                return False
-
-        prev = report[i]
+    diffs = [report[i] - report[i - 1] for i in range(1, len(report))]
+    increasing = diffs[0] > 0
+    for d in diffs:
+        if increasing and d < 0:
+            return False
+        if not increasing and d > 0:
+            return False
+        if not (1 <= abs(d) <= 3):
+            return False
 
     return True
+
+
+def remove_index(arr: list, index: int) -> list:
+    return arr[:index] + arr[index + 1:]
 
 
 if __name__ == '__main__':
