1af91d17bfea99e8f1ca5b2ec53468b55d10d606
lewcab <lewcabal@gmail.com>
2024-12-09 16:26:35 -0800

    day 5 part 2 done

--- day5/day5.py
+++ day5/day5.py
@@ -7,29 +7,29 @@ def main():
     rules, updates = parse_data(data)
 
     print(f"Part 1: {part1(rules, updates)}")
+    print(f"Part 2: {part2(rules, updates)}")
 
 
 def part1(rules: dict, updates: list) -> int:
     total = 0
 
     for u in updates:
-        broken = False
-        for i in range(1, len(u)):
-            curr_page = u[i]
-            page_rules = rules[curr_page]
-
-            for j in range(i):
-                # Check all previous pages if rules are broken
-                prev_page = u[j]
-                if prev_page in page_rules:
-                    broken = True
-                    break
-
-            if broken:
-                break
-
-        if not broken:
+        if is_update_correct(rules, u):
             total += u[len(u)//2]
+
+    return total
+
+
+def part2(rules: dict, updates: list) -> int:
+    total = 0
+
+    for u in updates:
+        if not is_update_correct(rules, u):
+            new_update = fix_update(rules, u)
+            if is_update_correct(rules, new_update):
+                total += new_update[len(new_update)//2]
+            else:
+                print(f"Failed to fix update: {u}")
 
     return total
 
@@ -58,6 +58,35 @@ def parse_data(data: list) -> tuple:
         i += 1
 
     return rules, updates
+
+
+def is_update_correct(rules: dict, update: list) -> bool:
+    for i in range(1, len(update)):
+        curr_page = update[i]
+        page_rules = rules[curr_page]
+
+        for j in range(i):
+            # Check all previous pages if rules are broken
+            prev_page = update[j]
+            if prev_page in page_rules:
+                return False
+
+    return True
+
+
+def fix_update(rules: dict, update: list) -> list:
+    # Cheese solution, but it works. Basically sorts the list by the number of effective rules
+    effective_rules = {}
+    update_set = set(update)
+    for u in update:
+        effective_rules[u] = rules[u].intersection(update_set)
+
+    new_update = [0 for _ in range(len(update))]
+    for u in update:
+        i = len(update) - len(effective_rules[u]) - 1
+        new_update[i] = u
+
+    return new_update
 
 
 if __name__ == '__main__':
